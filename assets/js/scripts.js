de160a7cc18f31632798205d42cc657ae094ca94
nancyhenry89 <nancyhenry89@gmail.com>
2018-10-21 17:19:52 -0500

    Add popup

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,5 +7,10 @@ $(document).ready(function(){
     var month = currentDate.getMonth()
     var year = currentDate.getFullYear()
     $('.date').text(  monthNames[month] + ", " +day + " "  + year + "")
-
+    $('.popup-title').click(function() {
+        $(this).siblings('.popup-cont').fadeIn();
+    });
+    $('.close').click(function() {
+        $(this).parents('.popup-cont').fadeOut();
+    });
 });
